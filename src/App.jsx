457d1701c6377b702df09144aebaa018c62ce4d1
khagendra62 <khagendraawasthi47@gmail.com>
2025-06-07 10:14:45 +0545

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ToDoList from "./components/ToDoList";
+
+const App = () => {
+  return (
+    <>
+      <ToDoList />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { FaAnglesUp, FaAnglesDown } from "react-icons/fa6";
+import "./ToDoList.css";
+const ToDoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const newTaskUp = [...tasks];
+      [newTaskUp[index], newTaskUp[index - 1]] = [
+        newTaskUp[index - 1],
+        newTaskUp[index],
+      ];
+      setTasks(newTaskUp);
+    }
+  }
+  function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const newTaskDown = [...tasks];
+      [newTaskDown[index + 1], newTaskDown[index]] = [
+        newTaskDown[index],
+        newTaskDown[index + 1],
+      ];
+      setTasks(newTaskDown);
+    }
+  }
+
+  return (
+    <>
+      <div className="to-do-list">
+        <h1>To-Do List </h1>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Enter a task"
+            value={newTask}
+            onChange={handleInputChange}
+            onKeyDown={(e) => {
+              if (e.key == "Enter") {
+                addTask();
+              }
+            }}
+          />
+          <button className="add-btn" onClick={addTask}>
+            Add
+          </button>
+        </div>
+
+        <ol>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span>{task}</span>
+              <button className="delete-btn" onClick={() => deleteTask(index)}>
+                Delete
+              </button>
+              <button
+                className="up-btn"
+                onClick={() => moveTaskUp(index)}
+                disabled={index === 0}
+              >
+                <FaAnglesUp />
+              </button>
+              <button
+                className="down-btn"
+                onClick={() => moveTaskDown(index)}
+                disabled={index === tasks.length - 1}
+              >
+                <FaAnglesDown />
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </>
+  );
+};
+
+export default ToDoList;
