26d85182512cef911be38f5952010c9880da3544
khagendra62 <khagendraawasthi47@gmail.com>
2025-06-08 00:11:25 +0545

    introduce local storage

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { FaAnglesUp, FaAnglesDown } from "react-icons/fa6";
 import "./ToDoList.css";
 const ToDoList = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+  const firstLoad = useRef(true);
+
+  useEffect(() => {
+    const savedTasks = localStorage.getItem("tasks");
+
+    console.log("Loaded from localStorage:", savedTasks);
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (firstLoad.current) {
+      firstLoad.current = false;
+      return;
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    console.log("Saved to localStorage:", tasks);
+  }, [tasks]);
 
   function handleInputChange(event) {
     setNewTask(event.target.value);
